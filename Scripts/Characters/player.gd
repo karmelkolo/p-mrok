22696a93be06240fec1635c62e44a4609ec7b8fe
karmelkolo <karmelkolo@gmail.com>
2025-02-24 21:25:39 +0100

    Poprawa animacji

--- Scripts/Characters/player.gd
+++ Scripts/Characters/player.gd
@@ -5,19 +5,11 @@ extends CharacterBody2D
 
 var facing = "Down"
 
-func _process(_delta: float) -> void:
-	if velocity != Vector2(0,0):
-		$Player_Sprite.animation = "Walking_" + facing
-	else:
-		$Player_Sprite.animation = "Standing_" + facing
-
 func _input(event) -> void:
 	if event.is_action_pressed("run"):
 		SPEED = SPEED * 2
-		$Player_Sprite.speed_scale = 2
 	if event.is_action_released("run"):
 		SPEED = SPEED / 2.0
-		$Player_Sprite.speed_scale = 1
 
 func _physics_process(_delta: float) -> void:
 	var direction_x := Input.get_axis("ui_left", "ui_right")
@@ -31,19 +23,10 @@ func _physics_process(_delta: float) -> void:
 		velocity.y = direction_y * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	if velocity != Vector2.ZERO:
+		$AnimationTree.set("parameters/Standing/blend_position", velocity)
+		$AnimationTree.set("parameters/Walking/blend_position", velocity)
+		
 		
 	move_and_slide()
-	
-	var velocity_abs = velocity.abs()
-	if velocity != Vector2(0,0):
-		if velocity_abs.x > velocity_abs.y:
-			if velocity.x > 0:
-				facing = "Right"
-			else:
-				facing = "Left"
-		else:
-			if velocity.y > 0:
-				facing = "Down"
-			else:
-				facing = "Up"
-				
