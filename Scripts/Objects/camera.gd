e1fc427243e90e0081bbc532ea873a17307a76e3
karmelkolo <karmelkolo@gmail.com>
2025-05-24 16:06:21 +0200

    Powierzchnia zrobiona

--- Scripts/Objects/camera.gd
+++ Scripts/Objects/camera.gd
@@ -1,9 +1,6 @@
 extends Camera2D
 
-var player : Node2D
-
-func _ready() -> void:
-	player = %Player
+@export var player : Node2D
 	
 func _physics_process(_delta: float) -> void:
 	transform = player.transform
