6d5ed1429f3ca2e817fac41f61bd1aa04975bcbc
Michał Kawecki Politechnika <255621@edu.p.lodz.pl>
2025-10-11 21:58:19 +0200

    interfejs dialogi

--- Scripts/Objects/safe.gd
+++ Scripts/Objects/safe.gd
@@ -2,11 +2,22 @@ extends Area2D
 
 var showInteractionLabel = false
 
+var dialogue_sprite: Sprite2D
+var dialogues = ["I tried everything... it won't budge.",
+"Tom had the key but now I don't know where it could be.",
+"But its contents Is the only thing that can help me escape."]
+var dialogue_index = 0
+
 func _process(_delta: float) -> void:
 	$Label.visible = showInteractionLabel
 	
-	#IMPLEMENTACJA DIALOGU DO SEJFU if showInteractionLabel && Input.is_action_just_pressed("interact"):
-		
+	#IMPLEMENTACJA DIALOGU
+	if not dialogue_sprite:
+		dialogue_sprite = get_tree().get_root().find_child("Dialogue", true, false)
+	if showInteractionLabel and Input.is_action_just_pressed("interact") and dialogue_sprite and dialogue_sprite.currently_playing == -1:
+		var hud = get_tree().get_root().find_child("HUD", true, false)
+		if hud:
+			hud.display_dialogue(self)
 
 func _on_body_entered(body):
 	if body is Player:
