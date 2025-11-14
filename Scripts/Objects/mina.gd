20d535b631b2897ae4a3cf0800fdd72068c01919
Marek Majchrowicz <117394413+UlekUlek@users.noreply.github.com>
2025-11-14 18:44:15 +0100

    mina wybucha i bones znika

--- Scripts/Objects/mina.gd
+++ Scripts/Objects/mina.gd
@@ -1,4 +1,5 @@
 extends Area2D
+
 	
 func _ready() -> void:
 	$Wybuch.play("idle")
@@ -8,7 +9,9 @@ func _on_body_entered(body: Node) -> void:
 	if body.name == "Player":
 		body.SPEED = 0
 		$Wybuch.play("explode")
-		$Minasprite.visible = false
+		$boom.play()
+		$Minasprite.visible = false 
+		body.visible = false
 		await $Wybuch.animation_finished
 		$Wybuch.play("idle")
 		get_tree().change_scene_to_file("res://Scenes/Smierc.tscn")
