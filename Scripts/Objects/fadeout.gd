773b5fb584f78eb3e32901f57deef8bce3a00176
Michał Kawecki Politechnika <255621@edu.p.lodz.pl>
2025-11-14 23:02:06 +0100

    niedzialajace lustro

--- Scripts/Objects/fadeout.gd
+++ Scripts/Objects/fadeout.gd
@@ -14,3 +14,15 @@ func fade_in():
 		var alpha: float = lerp(0.0, 1.0, elapsed / fade_time)
 		modulate.a = alpha
 		await get_tree().process_frame 
+		
+func fade_out():
+	var fade_time := 2.0 
+	var elapsed := 0.0
+
+	while elapsed < fade_time:
+		var delta := get_process_delta_time()
+		elapsed += delta
+		var alpha: float = lerp(1.0, 0.0, elapsed / fade_time)
+		modulate.a = alpha
+		await get_tree().process_frame 
+	visible = false
