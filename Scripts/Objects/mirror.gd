540928f6ce0747013c7534b24ebcff6770c75fd3
karmelkolo <karmelkolo@gmail.com>
2025-11-17 14:41:07 +0100

    Navmesh

--- Scripts/Objects/mirror.gd
+++ Scripts/Objects/mirror.gd
@@ -10,6 +10,9 @@ var dialogue_over = false
 
 var choice_index = 0 # 0 = A, 1 = B
 var choosing = false
+var block_input = false
+var mirror = false
+var block_choice = false
 	
 func _process(_delta: float) -> void:
 	$Label.visible = showInteractionLabel
@@ -17,26 +20,28 @@ func _process(_delta: float) -> void:
 	if not dialogue_sprite:
 		dialogue_sprite = get_tree().get_root().find_child("Dialogue", true, false)
 	if showInteractionLabel and Input.is_action_just_pressed("interact") and dialogue_sprite and dialogue_sprite.currently_playing == -1 and not dialogue_over:
+		mirror = true
 		var hud = get_tree().get_root().find_child("HUD", true, false)
 		if hud:
 			hud.display_dialogue(self)
+			choosing = false
 		
-	if dialogue_over and Input.is_action_just_pressed("continue"):
+	if dialogue_over and Input.is_action_just_pressed("continue") and not choosing and not block_choice and mirror:
+		mirror = false
 		choice()
-		#$"../HUD/HUD_Layer/Mirror/SEE".visible = true
-				
+		
+		
 func choice():
+	dialogue_over = false
+	choosing = true
 	%Player.can_move = false
 	$"../HUD/HUD_Layer/Mirror/FadeOut".fade_in()
+	block_input = true
 	await get_tree().create_timer(2.0).timeout
-	choosing = true
-	dialogue_over = false
+	block_input = false
 	
-	var hud = get_tree().get_root().find_child("HUD", true, false)
-	var see = hud.get_node("HUD_Layer/Mirror/A")
-	var leave = hud.get_node("HUD_Layer/Mirror/B")
-	see.visible = true
-	leave.visible = true
+	$"../HUD/HUD_Layer/Mirror/A".visible = true
+	$"../HUD/HUD_Layer/Mirror/B".visible = true
 	choice_visual()
 	
 func choice_visual():
@@ -52,13 +57,17 @@ func choice_visual():
 		leave.modulate = Color.WHITE
 
 func hide_choice():
-	var hud = get_tree().get_root().find_child("HUD", true, false)
-	hud.get_node("HUD_Layer/Mirror/A").visible = false
-	hud.get_node("HUD_Layer/Mirror/B").visible = false
+	$"../HUD/HUD_Layer/Mirror/A".visible = false
+	$"../HUD/HUD_Layer/Mirror/B".visible = false
 	
 func _see():
 	choosing = false
+	block_choice = true
+	block_input = true
 	hide_choice()
+	$"../HUD/HUD_Layer/Mirror/SEE".visible = true
+	$"../HUD/HUD_Layer/Mirror/FadeOut".fade_out()
+	await get_tree().create_timer(2.0).timeout
 	dialogues = ["It is not a wound. It is the rust of the soul.",
 				"The air feels like knifes on this growth.",
 				"I don't think I have that much time left before stage two.",
@@ -69,13 +78,36 @@ func _see():
 		var hud = get_tree().get_root().find_child("HUD", true, false)
 		if hud:
 			hud.display_dialogue(self)
+	await dialogue_over_wait()
+	_leave_see()
+
+func dialogue_over_wait():
+	while not dialogue_over:
+		await get_tree().process_frame
+		%Player.can_move = false
+	
+func _leave_see():
+	$"../HUD/HUD_Layer/Mirror/FadeOut".fade_in()
+	await get_tree().create_timer(2.0).timeout
+	$"../HUD/HUD_Layer/Mirror/SEE".visible = false
+	_leave()
 
 func _leave():
-	choosing = false
 	hide_choice()
+	choosing = false
+	block_choice = false
+	block_input = true
+	$"../HUD/HUD_Layer/Mirror/FadeOut".fade_out()
+	await get_tree().create_timer(2.0).timeout
 	%Player.can_move = true
+	dialogues = ["I do not bleed... But everything in me is dying.",
+					"I'm scared to look at it."]
+	block_input = false
+	
 	
 func _input(event):
+	if block_input:
+		return
 	if not choosing:
 		return
 	
